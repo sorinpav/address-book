9de53b8ea98c83d1252073d4a24e8d95bd28a57b
Constantin Pavelescu <sorinpavwebdev@gmail.com>
2019-08-16 21:30:09 +0100

    testing SSH Key

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 PORT = process.env.PORT || 5000;
 HELLO = 'github Test';
+HELLO2 = 'testing github';
 
 var server = app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)); 
 
